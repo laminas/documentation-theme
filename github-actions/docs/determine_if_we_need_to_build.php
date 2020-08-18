ed39c9d0bb242c96d6eb4d158e1357be202c604a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2020-08-18 08:48:50 -0500

    refactor: do not default to building latest release branch
    Do not automatically build if the release branch pushed is also the
    latest. Instead, we should build only if the release branch pushed is
    also the same one that reflects the latest stable tag.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- github-actions/docs/determine_if_we_need_to_build.php
+++ github-actions/docs/determine_if_we_need_to_build.php
@@ -2,10 +2,16 @@
 
 declare(strict_types=1);
 
+/*
+ * DECLARATIONS
+ */
 $ref   = getenv('GITHUB_REF');
 $repo  = getenv('GITHUB_REPOSITORY');
 $token = getenv('GITHUB_TOKEN');
 
+/*
+ * FUNCTIONS
+ */
 function buildDocs() {
     echo "TRUE";
     exit(0);
@@ -14,6 +20,13 @@ function buildDocs() {
 function skipDocs() {
     echo "FALSE";
     exit(0);
+}
+
+function compareRefNames(string $a, string $b): int
+{
+    $a = preg_replace('/\.x$/', '.0', $a);
+    $b = preg_replace('/\.x$/', '.0', $b);
+    return version_compare($a, $b);
 }
 
 /**
@@ -33,7 +46,7 @@ function executeApiCall(string $repo, string $resource, string $token): array
     $payload  = array_map(static function (string $ref): string {
         return $ref['name'];
     }, $payload);
-    usort($payload, $compareRefNames);
+    usort($payload, 'compareRefNames');
     return $payload;
 }
 
@@ -44,39 +57,27 @@ function extractMajorMinorVersion(string $version): string
     return $matches['release'] ?? $version;
 }
 
+/*
+ * MAIN
+ */
+
+// Assume that release branches are not in use if we detect a push to master
 if ('master' === $ref) {
-    // Assume that release branches are not in use if we detect a push to master
     buildDocs();
     exit(0); // redundant; placed here to note that buildDocs exits
 }
 
+// Not a release branch
 if (! preg_match('/^\d+\.\d+\.x$/', $ref)) {
-    // Not a release branch
     skipDocs();
     exit(0); // redundant; placed here to note that skipDocs exits
 }
 
-$compareRefNames = function ($a, $b) {
-    $a = preg_replace('/\.x$/', '.0', $a);
-    $b = preg_replace('/\.x$/', '.0', $b);
-    return version_compare($a, $b);
-};
-
-$branches = executeApiCall($repo, 'branches', $token);
-$branches = array_filter($branches, function ($branch) {
-    return (bool) preg_match('/^\d+\.\d+\.x$/', $branch);
-});
-$mostRecent = array_pop($branches);
-if ($mostRecent === $ref) {
-    buildDocs();
-    exit(0); // redundant; placed here to note that buildDocs exits
-}
-
-$tags = executeApiCall($repo, 'tags', $token);
-$committedReleaseVersion = extractMajorMinorVersion($ref);
+// Compare release branch name against latest tag name (by semver)
+$tags                     = executeApiCall($repo, 'tags', $token);
+$committedReleaseVersion  = extractMajorMinorVersion($ref);
 $mostRecentReleaseVersion = extractMajorMinorVersion(array_pop($tags));
-if ($committedReleaseVersion !== $mostRecentReleaseVersion) {
-    skipDocs();
-    exit(0); // redundant; placed here to note that skipDocs exits
-}
-buildDocs();
+
+$committedReleaseVersion === $mostRecentReleaseVersion
+    ? buildDocs()
+    : skipDocs();
